01c33b96c826776a1bb16d2608119723a546bb63
schoch <schoch@x1lcnc.x1lcnc.local>
2025-01-03 10:37:56 -0800

    updating parsing, using array instead of map

--- gramme_test_server/gramme_server.py
+++ gramme_test_server/gramme_server.py
@@ -11,23 +11,38 @@ import time
 print("Loading now pendant")
 print(f"Python version: {sys.version}")
 
+def parse_id(data):
+    id = data[2]
+    print(f"device id {id}")
+
+def parse_data(data):
+    msgtype = data[0]
+    match msgtype:
+        case True:
+            c['thetest-1'] =  not c['thetest-1']
+            print( "ping type was true")
+        
+        # data updates should match this
+        case False:
+            print (f"update: type was {data[1]}")
+            # encoder data should be like [False, b'e', 1, -8, -9]
+            updatetype = data[1]
+            if updatetype == b'e':
+                #encoder_list = data[b'e']
+                #update_encoders( data[b'e'])
+                parse_id(data)
+                print("encoder data")
+            elif updatetype == b'b':
+                print("btn data")
+            elif updatetype == b'p':
+                print("pot data")
+            else:
+                print( ' unknown data')
+        case _:
+            print( "doh")
+
 @gramme.server(8080,"192.168.11.3")
 #@gramme.server(8080)
 def my_awsome_data_handler(data):
-    print("got some data")
-    print( data)
-    #print( bin(data))
-    match data:
-        case {b'h':True}:
-            c['thetest-1'] =  not c['thetest-1']
-            print( "true")
-        case {b'h':False}:
-            print ('got some data')
-            if b'e' in data:
-                #encoder_list = data[b'e']
-                #update_encoders( data[b'e'])
-                print("encoder data")
-            else:
-                print( ' didnt find encoders')
-        case _:
-            print( "doh")
+    print(f"got some data\n\t{data}")
+    parse_data(data)    
