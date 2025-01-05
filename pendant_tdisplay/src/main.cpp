ad608180e7f156d1fd4415ba08d70982797cd2a6
bob <bob@bob.com>
2025-01-05 11:13:31 -0800

    display and adc working

--- pendant_tdisplay/src/main.cpp
+++ pendant_tdisplay/src/main.cpp
@@ -6,13 +6,21 @@
 
 #include <esp_now.h>
 #include <WiFi.h>
+#include <User_Setup_Select.h>
+#include <TFT_eSPI.h>
+TFT_eSPI tft = TFT_eSPI();
 
 
 #include <MsgPack.h>
 MsgPack::Packer packer;
 
-int enc1a = 13;
-int enc1b = 14;
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
+/* these pins clobber the t-display pins
+
 
 int enc2a = 15;
 int enc2b = 16;
@@ -22,12 +30,17 @@ int enc3b = 18;
 
 int enc4a = 19; // b
 int enc4b = 21;
+*/
+
+int enc1a = 12;
+int enc1b = 13;
 
 
 // Don't use 36,39 there are no pullups
 int btn1 = 32; // 
 int btn2 = 33;  // 
 
+int16_t adc0, adc1, adc2, adc3;
 
 ESP32Encoder encoder1;
 ESP32Encoder encoder2;
@@ -124,8 +137,9 @@ BtnInputData buttons[] = {b1,b2};
 
 
 
-#define NUM_ENC 4
-EncInputData encoders[] = {e1,e2,e3,e4};
+#define NUM_ENC 1
+//EncInputData encoders[] = {e1,e2,e3,e4};
+EncInputData encoders[] = {e1};
 
 
 void setupButtons() {
@@ -340,16 +354,76 @@ void setupPeer(){
   }
 }
 
+void mockupIdle(){
+
+  //tft.fillScreen(TFT_WHITE);
+  //tft.setRotation(3);  
+  tft.setCursor(0,0);
+  //tft.setTextSize(1);
+  float jog_scale = 0.0025;
+  float x_pos = 0.0;
+  float y_pos = 0.0;
+  float x_dtg = 1.0;
+  float y_dtg = 1.0;
+  String lcnc_state = "Idle";
+  tft.printf("js: %.4d\n",jog_scale);
+  tft.printf("x: %.4d\n", x_pos);
+  tft.printf("y: %.4d\n", y_pos);
+  tft.printf("xdtg: %.4d\n", x_dtg);
+  tft.printf("ydtg: %.4d\n", y_dtg);
+  tft.printf("lcnc state: %s\n", lcnc_state.c_str());
+ tft.println("foo\n" );
+ Serial.println("mock done");
+
+
+  
+}
+
+
+void setupDisplay(){
+  tft.init();
+  //tft.begin();
+  tft.fillScreen(TFT_BLACK);
+  if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+         //pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
+         //digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+         Serial.printf("TFT_BL is defined as %d\n", TFT_BL);
+         //analogWrite(TFT_BL, 128); // Set backlight brightness (0-255)
+
+    }
+
+  tft.setTextColor(TFT_WHITE,TFT_BLACK);
+  tft.setTextSize(1);
+  tft.setCursor(0,0);
+  tft.printf("Hello Setup! %d\n",10);
+  tft.println("foo bar");
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setRotation(3);
+  tft.setTextSize(1);
+
+  // Set the font
+  tft.setCursor(0,0,2);
+
+  tft.setTextColor(TFT_WHITE,TFT_BLACK,true);
+
+}
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin ( 115200 );
-
+  setupDisplay(); 
 
   ESP32Encoder::useInternalWeakPullResistors = puType::up;
   setupEnc(&encoder1,enc1a,enc1b); 
+  /*
   setupEnc(&encoder2,enc2a,enc2b); 
   setupEnc(&encoder3,enc3a,enc3b); 
   setupEnc(&encoder4,enc4a,enc4b); 
+  */
 
   setupButtons();
 
@@ -372,11 +446,38 @@ void setup() {
   readBtns.attach_ms(50,doReadButtons);
   //slowReadEncoders.attach_ms(1000,doReadSlow);
   setupPeer();
+
+
+  // THis didn't work
+  //TwoWire tw = TwoWire(1); // bus 1
+  //tw.setPins(21,17); // SDA, SCL
+  //tw.setClock(400000); // 400kHz clock speed
+  //tw.begin(); // Start I2C
+
+
+
+  //if (!ads.begin(0x48,&tw)) {
+  // use standard pins SDA 21 and SCL 22
+  if(!ads.begin()) {
+
+    Serial.println("Failed to initialize ADS.");
+  }
+  else{
+    Serial.println("ADS1115 initialized.");
+
+  }
   Serial.println("setup done");
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println("loop")
+  delay(1000);
+  mockupIdle();
+  adc0 = ads.readADC_SingleEnded(0);
+  float volts0 = ads.computeVolts(adc0);
+  Serial.printf("adc0: %d, volts: %f\n",adc0,volts0);
+
 }
 
